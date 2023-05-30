22150802dfd39a2c7dbdfd5bd941352eb7494b7b
Lewis Muthaura <lewislemmu10@gmail.com>
2023-05-30 16:24:31 +0300

    tailwind styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './index.css';
 import Employee from './components/Employee'
 import { useState } from "react";
 
@@ -10,18 +10,25 @@ function App() {
     return (
       <div className="App">
         {showEmployees ? (
-          <>
+    <>
           <input type="text" 
           onChange={(e) => {
             console.log (e.target.value);
             setRole(e.target.value);
           }} 
           />
-            <Employee name = 'Caleb' role = 'intern'/>
-            <Employee  name = 'Abby' role ={role}/>
-            <Employee  name = "John"/>
-            <Employee />
-          </>
+          <div className="flex flex-wrap justify-center">
+            <Employee name = 'Caleb' role = 'intern' img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg" />
+            <Employee  name = 'Abby' role ={role} img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+            <Employee  name = 'John' img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+            <Employee name = 'Caleb' role = 'intern' img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+            <Employee  name = 'Abby' role ={role} img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+            <Employee  name = 'John' img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+            <Employee name = 'Caleb' role = 'intern' img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+            <Employee  name = 'Abby' role ={role} img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+            <Employee  name = 'John' img = "https://wallpapers.com/images/hd/instagram-profile-pictures-ut4t3clnp7r84i2x.jpg"/>
+          </div>
+    </>
         ) : (
           <p>You cannot see the employees</p>
         )}
